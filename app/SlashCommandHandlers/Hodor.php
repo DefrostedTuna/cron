8eb4e87cfbf17e959837e8f30b5004b443f6c671
Rick Bennett <rbennett1106@gmail.com>
2017-07-29 20:01:40 -0400

    Formatted Hodor

--- app/SlashCommandHandlers/Hodor.php
+++ app/SlashCommandHandlers/Hodor.php
@@ -8,27 +8,25 @@ use Spatie\SlashCommand\Handlers\BaseHandler;
 
 class Hodor extends BaseHandler
 {
-/**
-* If this function returns true, the handle method will get called.
-*
-* @param \Spatie\SlashCommand\Request $request
-*
-* @return bool
-*/
-public function canHandle(Request $request): bool
-{
-    return starts_with($request->text, 'hodor');
-}
+    /*
+     * If this function returns true, the handle method will get called.
+     *
+     * @param \Spatie\SlashCommand\Request $request
+     * @return bool
+     */
+    public function canHandle(Request $request): bool
+    {
+        return starts_with($request->text, 'hodor');
+    }
 
-/**
-* Handle the given request.
-*
-* @param \Spatie\SlashCommand\Request $request
-*
-* @return \Spatie\SlashCommand\Response
-*/
-public function handle(Request $request): Response
-{
-return $this->respondToSlack("Hodor, hodor...");
-}
+    /*
+     * Handle the given request.
+     *
+     * @param \Spatie\SlashCommand\Request $request
+     * @return \Spatie\SlashCommand\Response
+     */
+    public function handle(Request $request): Response
+    {
+        return $this->respondToSlack("Hodor, hodor...");
+    }
 }
